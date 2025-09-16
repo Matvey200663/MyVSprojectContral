ac3f68af49ce78d1b005a60e7bad42f95e14b602
HappyMod <matvey200663@gmail.com>
2025-09-16 13:41:42 +0800

    Сделал главную форму

---
+++ FormMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class FormMain : Form
+    {
+        ИСПП6_Вар6_Агапов_АндреевEntities1 qwest;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            qwest = new ИСПП6_Вар6_Агапов_АндреевEntities1();
+
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var questions = qwest.Сотрудники.Select(i => i).ToList();
+            dataGridView1.DataSource = questions;
+        }
+    }
+}
