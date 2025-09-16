9aafaec049835a7f75de2e45aa5f9848037a2430
Overout1 <mark.aghapov.2000@mail.ru>
2025-09-16 08:41:23 +0300

    Добавил функцию перехода из авторизации в регистрацию Добавил функцию проверки существующего пользователя

--- FormAuthorization.Designer.cs
+++ FormAuthorization.Designer.cs
@@ -28,19 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.LabelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonToRegistration = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(83, 25);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(83, 78);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // LabelLogin
+            // 
+            this.LabelLogin.AutoSize = true;
+            this.LabelLogin.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelLogin.Location = new System.Drawing.Point(80, 9);
+            this.LabelLogin.Name = "LabelLogin";
+            this.LabelLogin.Size = new System.Drawing.Size(113, 13);
+            this.LabelLogin.TabIndex = 4;
+            this.LabelLogin.Text = "Логин (email)";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPassword.Location = new System.Drawing.Point(80, 62);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(85, 13);
+            this.labelPassword.TabIndex = 5;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(83, 104);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(100, 20);
+            this.buttonEnter.TabIndex = 2;
+            this.buttonEnter.Text = "Войти";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonToRegistration
+            // 
+            this.buttonToRegistration.Location = new System.Drawing.Point(83, 129);
+            this.buttonToRegistration.Name = "buttonToRegistration";
+            this.buttonToRegistration.Size = new System.Drawing.Size(100, 20);
+            this.buttonToRegistration.TabIndex = 3;
+            this.buttonToRegistration.Text = "Регистрация";
+            this.buttonToRegistration.UseVisualStyleBackColor = true;
+            this.buttonToRegistration.Click += new System.EventHandler(this.buttonToRegistration_Click);
             // 
             // FormAuthorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 288);
+            this.ClientSize = new System.Drawing.Size(262, 161);
+            this.Controls.Add(this.buttonToRegistration);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.LabelLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
             this.Name = "FormAuthorization";
             this.Text = "FormAuthorization";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label LabelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonToRegistration;
     }
 }
--- FormAuthorization.cs
+++ FormAuthorization.cs
@@ -12,9 +12,29 @@ namespace Project
 {
     public partial class FormAuthorization : Form
     {
+        ИСПП6_Вар6_Агапов_АндреевEntities1 qwest;
         public FormAuthorization()
         {
             InitializeComponent();
+            qwest = new ИСПП6_Вар6_Агапов_АндреевEntities1();
         }
+
+
+        private void buttonToRegistration_Click(object sender, EventArgs e)
+        {
+            FormRegister newForm = new FormRegister();
+            newForm.Show();
+            this.Hide();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+          bool exists = qwest.Пользователь.Any(i => i.ЭлектроннаяПочта == textBoxLogin.Text && i.Пароль == textBoxPassword.Text);
+            if (!exists)
+            {
+                MessageBox.Show("Такого пользователя нет");
+            }
+        }
+       
     }
 }
